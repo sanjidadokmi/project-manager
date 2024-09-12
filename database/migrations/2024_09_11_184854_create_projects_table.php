120aafe664ba9b80c1d4e69f3953a05f96d3fdf9
sanjidadokmi <sanjidadokmi@gmail.com>
2024-09-12 08:03:35 +0600

    .

---
+++ database/migrations/2024_09_11_184854_create_projects_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('Project Name');
+            $table->text('Project Description');
+            $table->enum('Status', ['ToDo', 'Processing', 'On Hold', 'Done'])->default('ToDo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
